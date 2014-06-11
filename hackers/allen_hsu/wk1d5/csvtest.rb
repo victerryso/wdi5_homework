236b97d1b6201e9b90f1a553dd88f90dce8966bc
Allen <0allen.hsu@gmail.com>
2014-06-11 10:58:04 +1000

    HappiTails

--- hackers/allen_hsu/wk1d5/csvtest.rb
+++ hackers/allen_hsu/wk1d5/csvtest.rb
@@ -3,9 +3,4 @@ require 'pry'
 require_relative 'client'
 
 client_list = []
-CSV.foreach("test.csv", {:headers => true, :header_converters => :symbol, }) do |row|
-  client_list << temp = Client.new(row[:name],row[:num_of_children],row[:age],row[:array])
-  binding.pry
-end
 
-p client_list
