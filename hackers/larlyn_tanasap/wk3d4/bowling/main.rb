a34160e1a7fd4e245772518617615a100a5d62ea
lyntco <strawww@gmail.com>
2014-06-20 09:23:49 +1000

    Add bowling and movie with database

---
+++ hackers/larlyn_tanasap/wk3d4/bowling/main.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'bowling.db'
+)
+
+require_relative 'models/game'
+require_relative 'models/frame'
+
+binding.pry
+
